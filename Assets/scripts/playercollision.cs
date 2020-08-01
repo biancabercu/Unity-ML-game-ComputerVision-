ba108640d0f4b81fcf212612acfc1f63e30ba33a
dianaandreea03 <diana@DESKTOP-C6QNCMT.localdomain>
2020-08-01 17:11:35 +0300

    proiect 2.0

---
+++ Assets/scripts/playercollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//rigid body + collider
+//GameObject.FindGameObjectsWithTag("Respawn"); //find object by tag
+//punem tag florilor
+public class playercollision : MonoBehaviour
+{
+    public Character_Animations_Human movement;
+
+    void OnCollisionEnter (Collision collisionInfo) {
+        if (collisionInfo.collider.name == "flower05") {
+           movement.enabled = false;
+            Debug.Log("++++++++++++++++++-----------------------" + collisionInfo.collider.name);
+        }
+        if (collisionInfo.collider.tag == "5") {
+            Debug.Log("avem tag 5==============================");
+        }
+        if (collisionInfo.collider.tag == "3") {
+            Debug.Log("avem tag 3==============================");
+        }
+        if (collisionInfo.collider.tag == "2") {
+            Debug.Log("avem tag 2==============================");
+        }
+        if (collisionInfo.collider.tag == "1") {
+            Debug.Log("avem tag 1==============================");
+        }
+        if (collisionInfo.collider.tag == "6") {
+            Debug.Log("avem tag 6==============================");
+        }
+    }
+    
+}
---
+++ Assets/scripts/score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public TextMeshProUGUI scoretext;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(player.position.z);
+        scoretext.text = player.position.z.ToString("0");
+    }
+}
