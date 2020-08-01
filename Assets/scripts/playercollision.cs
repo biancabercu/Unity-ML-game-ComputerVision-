fb29899c00287acd8de7b9262dbc90d356df6ffa
Dianaa98 <63339204+Dianaa98@users.noreply.github.com>
2020-08-01 23:50:37 +0300

    Scor

--- Assets/scripts/playercollision.cs
+++ Assets/scripts/playercollision.cs
@@ -1,34 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 //rigid body + collider
 //GameObject.FindGameObjectsWithTag("Respawn"); //find object by tag
 //punem tag florilor
 public class playercollision : MonoBehaviour
 {
     public Character_Animations_Human movement;
+    //public float points;
+    public int Score = 0;
+    public TextMeshProUGUI scoretext;
+    public Text ScoreText;
 
-    void OnCollisionEnter (Collision collisionInfo) {
-        if (collisionInfo.collider.name == "flower05") {
-           movement.enabled = false;
-            Debug.Log("++++++++++++++++++-----------------------" + collisionInfo.collider.name);
-        }
-        if (collisionInfo.collider.tag == "5") {
-            Debug.Log("avem tag 5==============================");
-        }
-        if (collisionInfo.collider.tag == "3") {
-            Debug.Log("avem tag 3==============================");
-        }
-        if (collisionInfo.collider.tag == "2") {
-            Debug.Log("avem tag 2==============================");
-        }
-        if (collisionInfo.collider.tag == "1") {
-            Debug.Log("avem tag 1==============================");
-        }
-        if (collisionInfo.collider.tag == "6") {
-            Debug.Log("avem tag 6==============================");
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(player.position.z);
+        scoretext.text = Score.ToString();
     }
+    void OnTriggerEnter (Collider collisionInfo)
+    {
+        if(collisionInfo.gameObject.tag == "1")
+        {
+            Destroy(collisionInfo.gameObject);
+            Score+=5;
+            //ScoreText.text = Score.ToString();
+        }
+        if (collisionInfo.gameObject.tag == "2")
+        {
+            Destroy(collisionInfo.gameObject);
+            Score += 2;
+           // ScoreText.text = Score.ToString();
+        }
+        if (collisionInfo.gameObject.tag == "3")
+        {
+            Destroy(collisionInfo.gameObject);
+            Score += 1;
+           //ScoreText.text = Score.ToString();
+        }
+        if (collisionInfo.gameObject.tag == "5")
+        {
+            Destroy(collisionInfo.gameObject);
+            Score -= 5;
+            //ScoreText.text = Score.ToString();
+        }
+        if (collisionInfo.gameObject.tag == "6")
+        {
+            Destroy(collisionInfo.gameObject);
+            Score -= 2;
+            //ScoreText.text = Score.ToString();
+        }
+        
+    }
+
     
 }
